03df98aeb3b4af0cf5581b93302114f89e3bfa87
bencode <bencode@163.com>
2024-07-12 19:00:21 +0800

    update

--- roam/use-react19/src/app.tsx
+++ roam/use-react19/src/app.tsx
@@ -2,6 +2,7 @@ import { App as LoadData } from './pages/c1-load-data'
 import { App as LoadDataSuspense } from './pages/c2-load-data-suspense'
 import { App as LoadDataSuspense2 } from './pages/c3-load-data-suspense'
 import { App as LoadDataSuspense3 } from './pages/c4-load-data-suspense'
+import { App as LoadDataSuspense4 } from './pages/c5-load-data'
 
 export function App() {
   return (
@@ -21,6 +22,10 @@ export function App() {
       <h2>Load data use Suspense refactor</h2>
       <div>
         <LoadDataSuspense3 />
+      </div>
+      <h2>Load data</h2>
+      <div>
+        <LoadDataSuspense4 />
       </div>
     </div>
   )
